24770a25ac310a2a10b5955e55a393a222309649
edugieun <gieun625@gmail.com>
2020-02-05 00:47:01 +0900

    감시

--- 풀이중/15683_boj_감시_testcase_only/15683_boj_surveillance.py
+++ 풀이중/15683_boj_감시_testcase_only/15683_boj_surveillance.py
@@ -4,6 +4,8 @@ sys.stdin = open('input.txt', 'r')
 dx = [0, 1, 0, -1, 0]
 dy = [-1, 0, 1, 0, -1]
 
+# 5번 카메라
+# 4방향으로 #으로 채운다.
 def surveil_5(row, col):
     for i in range(4):
         new_row = row + dy[i]
@@ -17,8 +19,10 @@ def surveil_5(row, col):
             new_row += dy[i]
             new_col += dx[i]
 
+# 4번 카메라
 def surveil_4(row, col):
-    # 방향 정하기
+    # 방향 정하기 -> 4번 카메라는 3방향을 감시한다.
+    # 따라서, 감시하지 않는 나머지 한 방향의 0의 개수가 가장 적은 곳만 제외한 나머지 3방향을 #으로 채운다.
     direction = 0
     min_zero = 99999
     for i in range(4):
@@ -36,7 +40,7 @@ def surveil_4(row, col):
             min_zero = zero_cnt
             direction = i
     
-    # 정한 방향으로 #채우기
+    # 한 곳을 제외한 나머지 방향 #채우기
     for i in range(4):
         if i == direction:
             continue
@@ -49,7 +53,8 @@ def surveil_4(row, col):
                 break
             new_row += dy[i]
             new_col += dx[i]
-
+            
+# 3번 카메라
 def surveil_3(row, col):
     # 각 방향 0개수 구하기
     zero_cnt_list = []
@@ -65,9 +70,11 @@ def surveil_3(row, col):
             new_row += dy[i]
             new_col += dx[i]
         zero_cnt_list.append(zero_cnt)
+    # 첫번째 0의 개수를 다시 넣어주는 이유는 1,2번 방향 / 2,3번 방향 / 3,4번 방향 / 4,1번 방향 중 선택할 것이기 때문
     zero_cnt_list.append(zero_cnt_list[0])
 
     # 방향 정하기
+    # 1,2번 방향 / 2,3번 방향 / 3,4번 방향 / 4,1번 방향. 각각의 두 방향의 0의 개수를 합한 것 중 최대값을 갖는 방향을 선택
     direction = 0
     max_zero = 0
     for i in range(len(zero_cnt_list) - 1):
@@ -88,17 +95,27 @@ def surveil_3(row, col):
                 new_row += dy[i]
                 new_col += dx[i]
 
-
+# 2번 카메라
 def surveil_2(row, col):
-    # 가로 또는 세로의 0개수 구하기
-    zero_cnt_ho = 0
-    zero_cnt_ver = 0
-    for i in matrix[row]:
-        if i == 0:
-            zero_cnt_ho += 1
-    for i in range(N):
-        if matrix[i][col] == 0:
-            zero_cnt_ver += 1
+    # 3번 카메라와 마찬가지로 4방향 모두 0의 개수를 저장
+    zero_cnt_list = []
+    for i in range(4):
+        zero_cnt = 0
+        new_row = row + dy[i]
+        new_col = col + dx[i]
+        while 0 <= new_row < N and 0 <= new_col < M:
+            if matrix[new_row][new_col] == 0:
+                zero_cnt += 1
+            elif matrix[new_row][new_col] == 6:
+                break
+            new_row += dy[i]
+            new_col += dx[i]
+        zero_cnt_list.append(zero_cnt)
+
+    # 가로와 세로의 '#' 개수
+    zero_cnt_ho = zero_cnt_list[1] + zero_cnt_list[3]
+    zero_cnt_ver = zero_cnt_list[0] + zero_cnt_list[2]
+
     # 방향 정하기
     if zero_cnt_ho > zero_cnt_ver:
         direction = 1
@@ -118,19 +135,38 @@ def surveil_2(row, col):
                 new_row += dy[i]
                 new_col += dx[i]
 
-
+# 1번 카메라
 def surveil_1(row, col):
+    direction = 0
+    max_zero = 0
+    # 0이 가장 많은 방향을 정한다.
     for i in range(4):
+        zero_cnt = 0
         new_row = row + dy[i]
         new_col = col + dx[i]
-
         while 0 <= new_row < N and 0 <= new_col < M:
             if matrix[new_row][new_col] == 0:
-                matrix[new_row][new_col] = '#'
+                zero_cnt += 1
             elif matrix[new_row][new_col] == 6:
                 break
             new_row += dy[i]
             new_col += dx[i]
+        if zero_cnt > max_zero:
+            max_zero = zero_cnt
+            direction = i
+
+    # 정한 방향 #로 채우기
+    for i in range(4):
+        if i == direction:
+            new_row = row + dy[i]
+            new_col = col + dx[i]
+            while 0 <= new_row < N and 0 <= new_col < M:
+                if matrix[new_row][new_col] == 0:
+                    matrix[new_row][new_col] = '#'
+                elif matrix[new_row][new_col] == 6:
+                    break
+                new_row += dy[i]
+                new_col += dx[i]
 
 N, M = map(int, input().split())
 
