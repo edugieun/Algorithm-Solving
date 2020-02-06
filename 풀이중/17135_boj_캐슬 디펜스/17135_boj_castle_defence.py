fe2c466fb120fe3132260354a605fbdb9976d3b7
임승현 <limseung10@gmail.com>
2020-02-06 19:43:14 +0900

    castel defence

--- 풀이중/17135_boj_캐슬 디펜스/17135_boj_castle_defence.py
+++ 풀이중/17135_boj_캐슬 디펜스/17135_boj_castle_defence.py
@@ -1,16 +1,36 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-def combination(com_arr, start):
+def combination(com_arr, start, left_enermies):
     if len(com_arr) == R:
-        print(com_arr)
+        targets = []
+        for arrow in com_arr:
+            dis_min = 9999999
+            for i in range(len(left_enermies)):
+                dis_row = N+1 - left_enermies[i][0]
+                dis_col = abs(left_enermies[i][1] - arrow)
+                dis = dis_row + dis_col
+                if dis <= D:
+                    if dis < dis_min:
+
+                    elif dis == dis_min:
+
+        print()
+
+
     else:
         for i in range(start, N):
-            combination(com_arr + [N_arr[i]], i + 1)
+            combination(com_arr + [N_arr[i]], i + 1, left_enermies)
 
 
 N, M, D = map(int, input().split())
 R = 3
 N_matrix = [list(map(int, input().split())) for i in range(N)]
 N_arr = [i for i in range(N)]
-combination([], 0)
+enermies = []
+for row in range(N):
+    for col in range(M):
+        if N_matrix[row][col] == 1:
+            enermies.append([row, col])
+combination([], 0, enermies)
+print(enermies)
